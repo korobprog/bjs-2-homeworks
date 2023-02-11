90c4ce67d1f8bc24f561e85b784469a52afa5591
korobprog <karobprog@gmail.com>
2023-02-11 20:50:30 +1000

    good

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,41 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    this.marks?.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (  !this.marks || !this.marks.length   ) {
+        return 0;
+    } 
+    return this.marks.reduce((acc, cur) => (acc + cur)) / this.marks.length;  
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage());
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
