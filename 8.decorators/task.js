d761675c3bfd679be8b2ecf8d40adc1ac84645a4
korobprog <karobprog@gmail.com>
2023-03-01 17:12:04 +1000

    decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,37 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+	function wrapper(...args) {
+		const hash = args.join(","); 
+		let objectInCache = cache.find((item) => item.hash === hash); 
+		if (objectInCache) { 
+			console.log("Из кэша: " + objectInCache.value);
+			return "Из кэша: " + objectInCache.value;
+		}
+		let result = func(...args); 
+		cache.push({ "hash": hash, "value": result });
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
-
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+function debounceDecoratorNew(func, ms) {
+	let timeout;
+	function wrapper(...args) {
+		wrapper.allCount++;
+		if (!timeout) {
+			func.apply(this, args);
+			wrapper.count++;
+		}
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			func.apply(this, args);
+			wrapper.count++;
+		}, ms);
+	}
+	wrapper.allCount = 0;
+	wrapper.count = 0;
+	return wrapper;
 }
